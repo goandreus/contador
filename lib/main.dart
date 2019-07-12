0ad99e17b657b23a8d1a20fefcd928f769c4e453
goandreus <andreshtml5@gmail.com>
2019-07-12 11:09:17 -0500

    widget center

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hola mundo'),
+      
+      home:Center(child: Text('Hola mundo'),)
     );
   }
 }
